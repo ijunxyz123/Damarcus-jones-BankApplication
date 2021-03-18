474bfe12d5cd3cf794e740c98da6d6f0555a55cf
Robertson otoo Asare <otoo.asare@amalitech.org>
2021-03-18 16:32:04 +0000

    Customer test completed

--- src/test/java/classes/CustomerTest.java
+++ src/test/java/classes/CustomerTest.java
@@ -6,20 +6,30 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class CustomerTest {
+    Customer customer;
 
     @Before
     public void setUp() throws Exception {
+        customer = new Customer("Thomas", 900.00 );
     }
+
 
     @Test
     public void getName() {
+        assertEquals("Thomas", customer.getName());
+        assertNotEquals("Sydney", customer.getName());
     }
 
+
     @Test
-    public void getTransactions() {
+    public void getTransaction() {
+        assertEquals(900.00, customer.getTransactions().get(0), 0.0);
+        assertNotEquals(150.00, customer.getTransactions().get(0), 0.0);
     }
 
     @Test
     public void addTransaction() {
+        customer.addTransaction(300.00);
+        assertEquals(2, customer.getTransactions().size());
     }
 }
