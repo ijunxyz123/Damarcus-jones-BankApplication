2964f3d554f95001dca8c8ba846acd799cb1ff3e
Robertson otoo Asare <otoo.asare@amalitech.org>
2021-03-18 15:59:23 +0000

    bank test done

--- src/test/java/classes/BankTest.java
+++ src/test/java/classes/BankTest.java
@@ -10,22 +10,34 @@ private Bank bank;
 
     @Before
     public void setUp() throws Exception {
-        bank = new Bank("TestBank");
+        bank = new Bank("National Australia Bank");
+        bank.addBranch("Adelaide");
+        bank.addCustomer("Adelaide", "Tim", 50.05);
+        bank.addCustomerTransaction("Adelaide", "Tim", 44.22);
+        bank.listCustomers("Adelaide", true);
     }
 
     @Test
     public void addBranch() {
+        assertTrue(bank.addBranch("Cape coast"));
+        assertFalse(bank.addBranch("Adelaide"));
     }
 
     @Test
     public void addCustomer() {
+        assertTrue(bank.addCustomer("Adelaide", "Bob", 300.00));
+        assertFalse(bank.addCustomer("Adelaide", "Tim", 50.05));
     }
 
     @Test
     public void addCustomerTransaction() {
+        assertTrue(bank.addCustomerTransaction("Adelaide", "Tim", 500.00));
+        assertFalse(bank.addCustomerTransaction("Colorado", "Manuel", 50.05));
     }
 
     @Test
     public void listCustomers() {
+        assertTrue(bank.listCustomers("Adelaide", true));
+        assertFalse(bank.listCustomers("Accra", false ));
     }
 }
